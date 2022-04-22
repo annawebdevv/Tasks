0a805582a8d2aa50b6de83430cd62777d696a704
egorovvaannaa <aegorova.exceedteam@gmail.com>
2022-04-22 12:02:34 +0300

    task-2

---
+++ task2/index.js
@@ -0,0 +1,33 @@
+// function sortByCount(arr){
+//     return arr
+//   }
+
+//   console.log(sortByCount(['apple','banana','peach','peach','grape','grape','apple','peach']))
+// ['peach','peach','peach','apple','apple','grape','grape','banana']
+
+let arr = [
+  "apple",
+  "banana",
+  "peach",
+  "peach",
+  "grape",
+  "grape",
+  "apple",
+  "peach",
+];
+let count = {};
+let result = [];
+
+for (let i = 0; i < arr.length; i++) {
+  let isntNew = arr[i] in count;
+
+  count[arr[i]] = isntNew ? ++count[arr[i]] : 1;
+}
+
+
+let countSorted = Object.entries(count).sort(
+  ([, valA], [, valB]) => valB - valA
+).flatMap(([a,b])=>Array.from({length:b},()=>a));
+
+console.log(countSorted);
+
